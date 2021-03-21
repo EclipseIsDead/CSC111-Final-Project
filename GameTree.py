1695c7608784d4a7a1b90d91ede7cd721c419afd
EclipseIsDead <sidmcheath@gmail.com>
2021-03-21 18:40:06 -0400

    Used Assignment 2 for quick GameTree

---
+++ GameTree.py
@@ -0,0 +1,98 @@
+"""
+This is the main game tree python file. This stores the tree, game tree and other relevant classes.
+"""
+from __future__ import annotations
+from typing import Optional
+
+GAME_START_MOVE = '*'
+
+
+class GameTree:
+    """A decision tree for L Game moves.
+
+    Each node in the tree stores an L Game move and a boolean representing whether
+    the current player (who will make the next move) is Red or Blue.
+
+    Instance Attributes:
+      - move: the current move (expressed in board notation), or '*' if this tree
+              represents the start of a game
+      - is_red_move: True if Red is to make the next move after this, False otherwise
+      - red_win_probability: the probability that white is going to win the game
+
+    Representation Invariants:
+        - self.move == GAME_START_MOVE or self.move is a valid L Game move
+        - self.move != GAME_START_MOVE or self.is_red_move == True
+        - 0.0 <= self.red_win_probability <= 1.0
+    """
+    move: str
+    is_red_move: bool
+    red_win_probability: float
+
+    # Private Instance Attributes:
+    #  - _subtrees:
+    #      the subtrees of this tree, which represent the game trees after a possible
+    #      move by the current player
+    _subtrees: list[GameTree]
+
+    def __init__(self, move: str = GAME_START_MOVE,
+                 is_red_move: bool = True, red_win_probability: float = 0.0) -> None:
+        """Initialize a new game tree.
+
+        Note that this initializer uses optional arguments, as illustrated below.
+
+        >>> game = GameTree()
+        >>> game.move == GAME_START_MOVE
+        True
+        >>> game.is_red_move
+        True
+        """
+        self.move = move
+        self.is_red_move = is_red_move
+        self._subtrees = []
+        self.red_win_probability = red_win_probability
+
+    def get_subtrees(self) -> list[GameTree]:
+        """Return the subtrees of this game tree."""
+        return self._subtrees
+
+    def find_subtree_by_move(self, move: str) -> Optional[GameTree]:
+        """Return the subtree corresponding to the given move.
+
+        Return None if no subtree corresponds to that move.
+        """
+        for subtree in self._subtrees:
+            if subtree.move == move:
+                return subtree
+
+        return None
+
+    def add_subtree(self, subtree: GameTree) -> None:
+        """Add a subtree to this game tree."""
+        self._subtrees.append(subtree)
+        self._update_red_win_probability()
+
+    def __str__(self) -> str:
+        """Return a string representation of this tree.
+        """
+        return self._str_indented(0)
+
+    def _str_indented(self, depth: int) -> str:
+        """Return an indented string representation of this tree.
+
+        The indentation level is specified by the <depth> parameter.
+        """
+        if self.is_white_move:
+            turn_desc = "Red's move"
+        else:
+            turn_desc = "Blue's move"
+        move_desc = f'{self.move} -> {turn_desc}\n'
+        s = '  ' * depth + move_desc
+        if self._subtrees == []:
+            return s
+        else:
+            for subtree in self._subtrees:
+                s += subtree._str_indented(depth + 1)
+            return s
+
+    def _update_red_win_probability(self):
+        pass
