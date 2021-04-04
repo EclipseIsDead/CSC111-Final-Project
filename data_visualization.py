7e534ac1e9bfb6cc77931231fd11abeaac779de3
EclipseIsDead <sidmcheath@gmail.com>
2021-04-03 22:30:55 -0400

    Fixed wierd formatting

--- data_visualization.py
+++ data_visualization.py
@@ -5,16 +5,16 @@ import csv
 
 
 def print_sample(games_file: str) -> None:
-    '''
+    """
     This is meant to be run on sample_game.csv to quickly look at it in string format.
     :param games_file:
     :return:
-    '''
+    """
     with open(games_file) as csv_file:
         reader = csv.reader(csv_file)
 
         for row in reader:
-           for i in range(len(row)):
-               if i % 4 == 0 and i != 0:
-                   print('\n')
-               print(row[i])
+            for i in range(len(row)):
+                if i % 4 == 0 and i != 0:
+                    print('\n')
+                print(row[i])
