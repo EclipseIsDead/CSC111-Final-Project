958f26eb43e1b262e2969d59a2ae4450941f3434
Bradley Mathi <bradley.mathi@mail.utoronto.ca>
2021-04-11 15:12:01 -0400

    Updated board.py, created draw_board and draw_pieces functions

--- board.py
+++ board.py
@@ -1,23 +1,34 @@
 import pygame
 from constants import *
 
-
 class Board:
     """This is the actual board object."""
     def __init__(self):
-        self.board = [STARTING_BOARD]
+        self.board = STARTING_BOARD
         self.selected_piece = None
 
-    def draw_squares(self, window):
-        window.fill(WHITE)
+    def draw_board(self, window):
+        window.fill(pygame.color.Color('white'))
 
         for row in range(ROWS):
             for col in range(COLS):
-                pygame.draw.rect(window, BLACK, (row * SQUARE_SIZE,
+                pygame.draw.rect(window, pygame.color.Color('black'), (row * SQUARE_SIZE,
                                                  col * SQUARE_SIZE,
                                                  SQUARE_SIZE,
-                                                 SQUARE_SIZE), 1)
+                                                 SQUARE_SIZE), LINE_THICC)
 
-    def create_board(self):
+    def draw_pieces(self, window):
         """We do this later"""
-        pass
+
+        for col in range(COLS):
+            for row in range(ROWS):
+                if self.board[col][row] == 'black':
+                    rad = SQUARE_SIZE // 2 - LINE_THICC
+                    center_pos = (col * SQUARE_SIZE + SQUARE_SIZE / 2, row * SQUARE_SIZE + SQUARE_SIZE / 2)
+                    pygame.draw.circle(window, pygame.color.Color('black'), center_pos, rad)
+                else:
+                    pygame.draw.rect(window, pygame.color.Color(self.board[col][row]), (row * SQUARE_SIZE + LINE_THICC,
+                                                     col * SQUARE_SIZE + LINE_THICC,
+                                                     SQUARE_SIZE - (2 * LINE_THICC),
+                                                     SQUARE_SIZE -(2 * LINE_THICC)), 0)
+
--- constants.py
+++ constants.py
@@ -1,10 +1,7 @@
 WIDTH, HEIGHT = 800, 800
 ROWS, COLS = 4, 4
 SQUARE_SIZE = WIDTH // ROWS
-
-# Colours (RGB)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+LINE_THICC = 1
 
 STARTING_BOARD = [['black', 'red', 'red', 'white'],
                   ['white', 'blue', 'red', 'white'],
