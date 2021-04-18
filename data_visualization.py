3f5f08dbf7fb2598b4776404a11eeb3a16004718
EclipseIsDead <sidmcheath@gmail.com>
2021-04-18 18:46:28 -0400

    outlined epic battle royale

--- data_visualization.py
+++ data_visualization.py
@@ -2,6 +2,7 @@
 This file is for visualizing the sample games and testing to see if the csv files are being read.
 """
 import csv
+from player import *
 
 
 def print_sample(games_file: str) -> None:
@@ -19,3 +20,10 @@ def print_sample(games_file: str) -> None:
                 if i % 4 == 0 and i != 0:
                     print('\n')
                 print(row[i])
+
+
+def battle_royal(player1: Player, player2: Player) -> None:
+    """
+    This function has to AI's play against each other and plots the result of their games.
+    """
+    return None
